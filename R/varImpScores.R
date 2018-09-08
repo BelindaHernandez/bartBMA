792ced0575d7257da7856e3860b8a1922a9af060
Eoghan O'Neill <oneill.eoghan.1@gmail.com>
2018-09-08 20:34:05 +0100

    Corrected varImpScores to refer to weighted variable split counts. Corrected varIncProb to give posterior inclusion probabilities.

--- R/varImpScores.R
+++ R/varImpScores.R
@@ -1,10 +1,12 @@
 varImpScores<-function(object,...){
   #object will be bartBMA object.
   imp_vars2=get_weighted_var_imp(num_vars=object$numvars,BIC=object$bic,sum_trees=object$sumoftrees)
-  res<-apply(imp_vars2[[3]],2,sum)
-  class(res)<-"varImpScores.bartBMA"  
-  res
+  res<-apply(imp_vars2[[4]],2,sum)
+  #create varImpPlot command
+  vIP<-rep(NA,length(res))
+  total_weighted_var_counts<-sum(res)
+  #now get variable inclusion probabilities
+  vIP<-res/total_weighted_var_counts
+  class(vIP)<-"varImpScores.bartBMA"
+  vIP
 }
-
-
-
