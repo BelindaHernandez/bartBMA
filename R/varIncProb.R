af883a4dc9838b30fdec8aafab1a2cf9a5c43f29
BelindaHernandez <belindahernandez25@gmail.com>
2018-11-20 17:31:57 +0000

    Update varIncProb.R

--- R/varIncProb.R
+++ R/varIncProb.R
@@ -1,6 +1,6 @@
 varIncProb<-function(object,...){
   imp_vars2=get_weighted_var_imp(num_vars=object$numvars,BIC=object$bic,sum_trees=object$sumoftrees)
-  res<-apply(imp_vars2[[3]],2,sum)
+  res<-apply(imp_vars2[[4]],2,sum)
   #create varImpPlot command
   vIP<-rep(NA,length(res))
   total_weighted_var_counts<-sum(res)
