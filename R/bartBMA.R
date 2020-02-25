4912949b0584f998abe2154d373804e72fe44d95
epo21@ad.econ.cam.ac.uk <epo21@ad.econ.cam.ac.uk>
2020-02-25 12:00:03 +0000

    Updated description file. New version of Roxygen2, small changes to help documentation. Added example to bartBMA.R documentation.

--- R/bartBMA.R
+++ R/bartBMA.R
@@ -56,6 +56,21 @@
 #' @importFrom Rdpack reprompt 
 #' @importFrom Rcpp evalCpp
 #' @importFrom stats sd qchisq quantile qnorm qchisq pnorm
+#' @examples
+#'N <- 100
+#'p<- 100
+#'set.seed(100)
+#' library(bartBMA)
+#'epsilon <- rnorm(N)
+#' xcov <- matrix(runif(N*p), nrow=N)
+#' y <- sin(pi*xcov[,1]*xcov[,2]) + 20*(xcov[,3]-0.5)^2+10*xcov[,4]+
+#' 5*xcov[,5]+epsilon
+#' epsilontest <- rnorm(N)
+#' xcovtest <- matrix(runif(N*p), nrow=N)
+#' ytest <- sin(pi*xcovtest[,1]*xcovtest[,2]) + 20*(xcovtest[,3]-0.5)^2+10*xcovtest[,4]+
+#' 5*xcovtest[,5]+epsilontest
+#' bart_bma_example <- bartBMA(x.train = xcov,y.train=y,x.test=xcovtest,zero_split = 1, 
+#' only_max_num_trees = 1,split_rule_node = 0)
 
 
 bartBMA<-function(x.train,...)UseMethod("bartBMA")
