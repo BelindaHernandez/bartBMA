7fe891cccea27ba145f8c0ef3c39fceacadc762e
epo21@ad.econ.cam.ac.uk <epo21@ad.econ.cam.ac.uk>
2020-05-27 21:19:25 +0100

    removed some lines from documentation. Removed unnecessary test data input frpm a few functions

--- R/varIncProb.R
+++ R/varIncProb.R
@@ -28,6 +28,8 @@ varIncProb<-function(object){
   #object will be bartBMA object.
   imp_vars2=get_weighted_var_imp(num_vars=object$numvars,BIC=object$bic,sum_trees=object$sumoftrees)
   res<-apply((imp_vars2[[3]]>0)*imp_vars2[[1]],2,sum)
+  #res<- t(imp_vars2[[3]]>0)%*%imp_vars2[[1]]
+  
   #create varImpPlot command
   class(res)<-"varIncProb.bartBMA"
   res
